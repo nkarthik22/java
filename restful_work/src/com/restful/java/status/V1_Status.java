65ef77b36b4ac421d01aadead75ec4c79345b9dc
nkarthik22 <nkarthik22@hotmail.com>
2014-04-21 20:18:33 +0530

    updates

--- restful_work/src/com/restful/java/status/V1_Status.java
+++ restful_work/src/com/restful/java/status/V1_Status.java
@@ -3,6 +3,8 @@ package com.restful.java.status;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jettison.json.JSONObject;
+
 import com.restful.java.vo.ResultVO;
 
 @Path("/v1/status/")
@@ -14,15 +16,26 @@ public class V1_Status {
 		return "<p>Java Restful Web Service<p>";
 	}
 	
-	@Path("/version")
+	@Path("/version/{versionno}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String returnVersion() {
+	public String returnVersion(@PathParam("versionno") String versionno) {
+		JSONObject jsobject = new JSONObject();
+		
 		ResultVO resultVO = new ResultVO();
+		resultVO.setVersion(versionno);
 		resultVO.setName("Karthik");
 		resultVO.setAge(36);
 		resultVO.setAddress("16 Arumugam St, puzhuthivakkam, chennai-91");
 		return resultVO.toString();
+	}
+	
+	@Path("users/{username: [a-zA-Z][a-zA-Z_0-9]*}")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getUserName(@PathParam("username") String userName, @QueryParam("country") String country) {
+		System.out.println("Inside this method");
+		return "Username : "+userName+"Country:"+country;
 	}
 	
 }
--- restful_work/src/com/restful/java/vo/ResultVO.java
+++ restful_work/src/com/restful/java/vo/ResultVO.java
@@ -6,6 +6,16 @@ public class ResultVO {
 	private String name="";
 	private int age;
 	private String address="";
+	private String version;
+	public String getVersion() {
+		if (this.version==null) {
+			this.version="";
+		}
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
 	public String getName() {
 		if (this.name==null) {
 			this.name="";
@@ -31,5 +41,8 @@ public class ResultVO {
 		this.address = address;
 	}
 	
-	
+
+	public String toString() {
+		return this.getName()+":"+this.getAge()+":"+this.getAddress()+":"+this.getVersion();
+	}
 }
